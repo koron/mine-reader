9fe9c05f7d6e81b19172ba0182c4e1a5a4d0d32c
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-05-02 22:50:26 +0900

    impl skeleton of handlers

--- feed-server/lib/feed_server.js
+++ feed-server/lib/feed_server.js
@@ -4,10 +4,21 @@ module.exports = FeedServer;
 
 var http = require('http');
 var domain = require('domain');
+var url = require('url');
+
+var PREFIX = '/feed/users/';
 
 function FeedServer(config)
 {
   var httpServer = http.createServer(handleRequest2);
+  var resourceHandlers = {
+    'subscriptions': handleSubscriptions,
+    'categories': handleCategories,
+    'feeds': handleFeeds,
+    'unreads': handleUnreads,
+    'favorites': handleFavorites,
+  };
+
   this.config = config;
   this.httpServer = httpServer;
 
@@ -24,13 +35,6 @@ function FeedServer(config)
     });
   }
 
-  function handleRequest(request, response)
-  {
-    // TODO:
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello World');
-  }
-
   function handleError(request, response, err)
   {
     try {
@@ -40,6 +44,75 @@ function FeedServer(config)
       consle.error('Error handling error', e);
       request.domain.dispose();
     }
+  }
+
+  function handleRequest(request, response)
+  {
+    // parse addressing.
+    var parsed = url.parse(request.url, true);
+    var pathname = parsed.pathname;
+    var query = parsed.query;
+    // filter unknown path.
+    if (pathname.indexOf(PREFIX) != 0) {
+      response.writeHead(404);
+      response.end('Not found (code:0)');
+      return;
+    }
+    // extract args (userId, handler) from pathname.
+    var args = pathname.split('/').slice(3);
+    if (args.length < 2) {
+      response.writeHead(404);
+      response.end('Not found (code:1)');
+      return;
+    }
+    var userId = args[0];
+    var handler = resourceHandlers[args[1]];
+    if (!handler) {
+      response.writeHead(404);
+      response.end('Not found (code:2)');
+      return;
+    }
+    handler(request, response, userId, query);
+  }
+
+  function handleSubscriptions(request, response, user, query)
+  {
+    // TODO:
+    console.log('user=%s query=%s', user, JSON.stringify(query));
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World: handleSubscriptions');
+  }
+
+  function handleCategories(request, response, user, query)
+  {
+    // TODO:
+    console.log('user=%s query=%s', user, JSON.stringify(query));
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World: handleCategories');
+  }
+
+  function handleFeeds(request, response, user, query)
+  {
+    // TODO:
+    console.log('user=%s query=%s', user, JSON.stringify(query));
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World: handleFeeds');
+  }
+
+  function handleUnreads(request, response, user, query)
+  {
+    // TODO:
+    console.log('user=%s query=%s', user, JSON.stringify(query));
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World: handleUnreads');
+  }
+
+  function handleFavorites(request, response, user, query)
+  {
+    // TODO:
+    console.log('user=%s query=%s', user, JSON.stringify(query));
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World: handleFavorites');
   }
 }
 

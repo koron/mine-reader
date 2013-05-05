0a4c5b5a4d39ee466e7d9526ba55c911a36b90f7
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-05-05 13:57:20 +0900

    add feedparser

--- feed-crawler/lib/site.js
+++ feed-crawler/lib/site.js
@@ -3,6 +3,8 @@
 module.exports = Site;
 
 var http = require('http');
+var util = require('util');
+var FeedParser = require('feedparser');
 
 function Site(url)
 {
@@ -22,7 +24,27 @@ Site.prototype.fetch = function(callback)
   {
     var items = [];
     console.log('Got response %d from %s', res.statusCode, url);
+
     // TODO:
+    res.pipe(new FeedParser())
+      .on('error', function(error) {
+        // always handle errors
+        console.log('on_error: %s', util.inspect(error));
+      })
+    .on('meta', function (meta) {
+      // do something
+      console.log('on_meta: %s', JSON.stringify(meta));
+    })
+    .on('article', function (article) {
+      // do something else
+      console.log('on_article: %s', article.title);
+      //JSON.stringify(article));
+    })
+    .on('end', function () {
+      // do the next thing
+      console.log('on_end');
+    });
+
     callback(items);
   }
 
